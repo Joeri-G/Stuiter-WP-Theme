6fe7534241e6518ce188b45c1389d95ff768fadf
Joeri <3677joeri@gmail.com>
2020-07-24 23:13:47 +0200

    Added very important and absolutely neccesary changes to theme

--- header.php
+++ header.php
@@ -4,5 +4,6 @@
   <meta charset="utf-8">
   <title>Theater Stuiter</title>
   <?php wp_head(); /* hook for custom plugins */ ?>
+  <!-- Theme Developed By Joeri Geuzinge (https://www.joerigeuzinge.nl) -->
 </head>
 <body <?php body_class(); ?>>
