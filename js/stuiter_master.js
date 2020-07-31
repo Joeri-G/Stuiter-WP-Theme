25625a791795abb799b27f055365df040e77ec21
Joeri-G <3677joeri@gmail.com>
2020-07-31 15:26:15 +0200

    Added js to sync height of html elements with css class `sync-height`

--- js/stuiter_master.js
+++ js/stuiter_master.js
@@ -1,4 +1,23 @@
+'use strict';
 document.body.onload = () => {
   // log credits into the console when the page has finished loading
   console.log("This Wordpress theme was created by Joeri Geuzinge (https://www.joerigeuzinge.nl)");
+  // sync height
+  syncHeight();
+}
+
+/**
+ * This function syncs the height of every element with a `sync-height` css class
+*/
+function syncHeight() {
+  const elementsToSync = document.querySelectorAll(".sync-height");
+  let max_height = 0;
+  for (const el of elementsToSync) {
+    // get the largest element height
+    if (el.clientHeight > max_height) max_height = el.clientHeight;
+  }
+  for (const elNewHeight of elementsToSync) {
+    // set the height to the largest height
+    elNewHeight.style.height = `${max_height.toString()}px`;
+  }
 }
